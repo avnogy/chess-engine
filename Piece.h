1de8a39f05854ce10769c6ae550c85feef78fc2c
avnogy <48830273+avnogy@users.noreply.github.com>
2021-12-29 19:38:02 +0200

    declared Piece class

--- Piece.h
+++ Piece.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Utility.h"
+
+class Piece
+{
+public:
+	//constructor and destructor.
+	Piece();
+	~Piece();
+
+	//checks if the source and destination squares are equal.
+	bool boardLegality(const string input);
+
+	//checks if destination square is according to piece movement.
+	//this function is created for each piece type.
+	virtual bool pieceLegality(const string input) = 0;
+
+	string _position;
+	char _pieceType;
+};
