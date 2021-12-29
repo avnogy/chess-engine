8ef88c05231a706d55c2745485988e2ac33bd968
avnogy <48830273+avnogy@users.noreply.github.com>
2021-12-29 19:50:50 +0200

    implemented piece class

---
+++ Piece.cpp
@@ -0,0 +1,42 @@
+#include "Piece.h"
+
+Piece::Piece(char type, string position):_pieceType(type), _position(position)
+{
+}
+
+Piece::~Piece()
+{
+}
+
+bool Piece::boardLegality(const string input)
+{
+	//TODO: call all engine functions to check position 
+	return false;
+}
+
+bool Piece::getPieceColor()
+{
+	//TODO: check for piece color
+	return false;
+}
+
+string Piece::getPosition() const
+{
+	return this->_position;
+}
+
+void Piece::setPosition(const string new_position)
+{
+	this->_position = new_position;
+}
+
+char Piece::getPieceType() const
+{
+	return this->_pieceType;
+}
+
+void Piece::setPieceType(const char new_type)
+{
+	this->_pieceType = new_type;
+}
+
--- Piece.h
+++ Piece.h
@@ -5,7 +5,7 @@ class Piece
 {
 public:
 	//constructor and destructor.
-	Piece();
+	Piece(char type,string position);
 	~Piece();
 
 	//checks if the source and destination squares are equal.
@@ -15,11 +15,14 @@ public:
 	//this function is created for each piece type.
 	virtual bool pieceLegality(const string input) = 0;
 
+	//returns piece color
+	bool getPieceColor();
+
 	//getters and setters
 	string getPosition() const;
-	string setPosition(const string new_position);
+	void setPosition(const string new_position);
 	char getPieceType() const;
-	char setPieceType(const char new_type);
+	void setPieceType(const char new_type);
 
 private:
 	string _position;
