d5bfce49e24232be1f6fba0e4542f85bbe9896e9
avnogy <48830273+avnogy@users.noreply.github.com>
2021-12-30 21:52:42 +0200

    implemented isSrcPiece

--- Engine.cpp
+++ Engine.cpp
@@ -1,2 +1,13 @@
 #include "Engine.h"
 
+bool Engine::isSrcPiece(Game &game,string location)
+{
+	//double indexes = utility::stringToIndexes(location);
+	//int row = indexes;
+	//int col = int(indexes * 10) % 10;
+	int row = 0, col = 0;
+	utility::stringToIndexes(location,row,col);
+	char pType = game.board[row][col]->getPieceType();
+	return pType != '#';
+}
+
