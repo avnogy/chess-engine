ebabcda0c9ad1799c1298d0bea3c9078e70fd173
Nir Amir <nir.amir123@gmail.com>
2021-12-30 22:33:27 +0200

    implemented pieceLegality for class Knight

--- Knight.cpp
+++ Knight.cpp
@@ -11,3 +11,14 @@ Knight::Knight(bool isWhite, string position) : Piece('n', position)
 Knight::~Knight()
 {
 }
+
+// im really not sure how the horsey moves tbh
+bool Knight::pieceLegality(string position) const
+{
+	string destination;
+	string source;
+	utility::separateMove(position, source, destination);
+
+	return abs(destination[0] - source[0]) == 2 && abs(destination[1] - source[1]) == 1 ||
+		abs(destination[0] - source[0]) == 1 && abs(destination[1] - source[1]) == 2;
+}
