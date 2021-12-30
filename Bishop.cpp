8a01d2c3d0ca3494504e8c1b927b3f8dcd7e21e3
Nir Amir <nir.amir123@gmail.com>
2021-12-30 23:00:32 +0200

    implemented pieceLegality for class Bishop

--- Bishop.cpp
+++ Bishop.cpp
@@ -11,3 +11,14 @@ Bishop::Bishop(bool isWhite, string position) : Piece('b', position)
 Bishop::~Bishop()
 {
 }
+
+// returns true if destination is located diagonally from the source
+bool Bishop::pieceLegality(string position) const
+{
+	string destination;
+	string source;
+	utility::separateMove(position, source, destination);
+	// if the difference between each coordinate of destination and source is equal
+	// then they are located diagonally from each other
+	return abs(destination[0] - source[0]) == abs(destination[1] - source[1]);
+}
