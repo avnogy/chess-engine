5f70f8d0afcc9568bec2551a30f7aceb5146227a
avnogy <48830273+avnogy@users.noreply.github.com>
2021-12-30 21:09:19 +0200

    throw exception if indexes are invalid

--- Utility.cpp
+++ Utility.cpp
@@ -5,7 +5,10 @@ namespace utility
 	string indexesToString(int row, char col)
 	{
 		string locationString = "";
-		// TODO: throw exception if either row or col are invalid
+		if (row > 8 || row < 0 || col > 8 || col < 0)
+		{
+			throw "Invalid index for position string.";
+		}
 		locationString += col;
 		locationString += row + '0';
 		return locationString;
