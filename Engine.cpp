b37aa15560c043622f0f85001159afefd27326c6
avnogy <48830273+avnogy@users.noreply.github.com>
2021-12-30 22:21:13 +0200

    fixed whole location to src and dst in engine

--- Engine.cpp
+++ Engine.cpp
@@ -2,19 +2,20 @@
 
 bool Engine::isSrcPiece(Game &game,string location)
 {
-	//double indexes = utility::stringToIndexes(location);
-	//int row = indexes;
-	//int col = int(indexes * 10) % 10;
+	string src = "", dst = "";
+	utility::separateMove(location, src, dst);
 	int row = 0, col = 0;
-	utility::stringToIndexes(location,row,col);
+	utility::stringToIndexes(src,row,col);
 	char pType = game.board[row][col]->getPieceType();
 	return pType != '#';
 }
 
 bool Engine::isDstOccupied(Game& game, string location)
 {
+	string src = "", dst = "";
+	utility::separateMove(location, src, dst);
 	int row = 0, col = 0;
-	utility::stringToIndexes(location, row, col);
+	utility::stringToIndexes(dst, row, col);
 	char pType = game.board[row][col]->getPieceType();
 	return (pType != '#' && game.board[row][col]->getPieceColor() == game.currentPlayer);
 }
