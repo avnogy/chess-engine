29cdd8d62a95e78f9316af218a317a679ea78e00
avnogy <48830273+avnogy@users.noreply.github.com>
2021-12-30 21:23:00 +0200

    created engine class

--- Engine.h
+++ Engine.h
@@ -1,1 +1,13 @@
 #include "Utility.h"
+#include "Game.h"
+
+class Engine
+{
+public:
+	bool isSrcPieces(string);
+	bool isDstOccupied(string);
+	bool doesCauseDiscoverey(string);
+	bool isOutOfBounds(string);
+	bool AreIndexesEqual(string);
+	bool IsCheckmate(string);
+};
