d7f7020c122b61f795e3ace088584738d7608ddb
Nir Amir <nir.amir123@gmail.com>
2021-12-30 22:24:04 +0200

    implemented PieceLegality for class King

--- King.cpp
+++ King.cpp
@@ -11,3 +11,14 @@ King::King(bool isWhite, string position) : Piece('k', position)
 King::~King()
 {
 }
+
+// returns true if position is next to the current position
+bool King::pieceLegality(string position) const
+{
+	string destination;
+	string source;
+	utility::separateMove(position, source, destination);
+
+	// returns true if difference between row, column or both is 1
+	return abs(source[0] - destination[0]) == 1 || abs(source[1] - destination[1]) == 1;
+}
