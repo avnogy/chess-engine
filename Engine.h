f29a4838aee8c351625d8f821835d40e7475f7f1
avnogy <48830273+avnogy@users.noreply.github.com>
2022-01-01 22:24:04 +0200

    added forward declaration of Game class in Engine.h

--- Engine.h
+++ Engine.h
@@ -2,14 +2,14 @@
 #include "Utility.h"
 #include "Game.h"
 #include "Piece.h"
-
+class Game;
 class Engine
 {
 public:
 	//utility functions
 	static bool isCheckmate(Game& game, string location);
-	
-	static bool boardLegality(Game& game, string location,bool isHorse);
+
+	static bool boardLegality(Game& game, string location, bool isHorse);
 
 	//board validity functions
 	static bool isSrcPiece(Game& game, string location);
